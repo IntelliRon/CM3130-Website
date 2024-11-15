58eafd0206c7c299c33209d58c97a7c05382922b
IntelliRon <gulpsabook@gmail.com>
2024-11-15 16:54:52 +0000

    Added profile post implementation
    Added server version control. Starts at v0.0.1 now
    
    Added validation to ensure that a database username and password is actually given
    
    Fixed providing failure reasons when redirecting on profile creation or login
    
    Cleaned up some unused imports in package.json

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
+/**
+ * Placement Hub Server
+ * v.0.0.1
+ * 
+ * 01/11/2024 - 15/11/2024
+ */
+const VERSION = "v0.0.1";
+
+
 var fs = require('fs');
 var util = require('util');
 var log_file = fs.createWriteStream(__dirname + '/debug.log', {flags : 'w'});
 var log_stdout = process.stdout;
 
 // Override the console.log method to write to a debug file instead
-console.log = function(d) { //
+console.log = function(d) {
   log_file.write(new Date().toISOString() + " - " + util.format(d) + '\n');
   log_stdout.write(new Date().toISOString() + " - " + util.format(d) + '\n');
 };
@@ -12,14 +21,22 @@ console.error = console.log;
 
 
 
+// Print some debug info
+console.log("Server is initialising...");
+console.log("Running on server version " + VERSION);
 
-console.log("Server is initialising...")
+const SERVER_PORT = process.env.PORT | 8080; // Default port to 8080
 
+// Get all environment variables
 require('dotenv').config();
-
-const SERVER_PORT = process.env.PORT | 8080;
-const DB_PASSWORD = process.env.DATABASE_PASSWORD;
 const { MongoClient, ServerApiVersion } = require('mongodb'); // npm install mongodb
+
+if (!process.env.DATABASE_USERNAME || !process.env.DATABASE_PASSWORD) {
+    console.log("Cannot retrieve database username or password. Please set the database username and password in the file '.env'");
+    console.log("Server initialisation stopped");
+    return; // Return stops the server
+}
+
 const url = "mongodb+srv://" + process.env.DATABASE_USERNAME + ":" + process.env.DATABASE_PASSWORD + "@cluster0.vtqin.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(url, {
     serverApi: {
@@ -57,17 +74,13 @@ async function connectDB() {
 // Tell express to allow use of the public folder for things such as scripts
 app.use(express.static('public'));
 
+
+
 /* GET Routes */
 
-app.get("/", /*async*/ function(req, res) {
+app.get("/", function(req, res) {
+    // Render the homepage
     res.render("pages/index");
-
-    /* DATABASE TESTING */
-
-    // col = db.collection("comments");
-    // console.log("Got collection");
-    // result = await col.findOne();
-    // res.send(result);
 });
 
 app.get("/forum/placements", function(req, res) {
@@ -90,11 +103,6 @@ app.get("/register", function(req, res) {
     // Registration page
 });
 
-app.get("/profile", function(req, res) {
-    res.render("pages/dev");
-    // Profile viewing page
-});
-
 app.get("/profile/edit", function(req, res) {
     res.render("pages/dev");
     // Profile editing page
@@ -103,6 +111,7 @@ app.get("/profile/edit", function(req, res) {
 
 
 /* POST Routes */
+
 app.post("/loadthreads", function(req, res) {
     res.send("Hmm. You shouldn't be here! This page is still under development");
     // Sideload all the threads here (remember to use pagination. Don't load ALL posts at once)
@@ -122,9 +131,9 @@ app.post("/dologin", async function(req, res) {
     let redirectURL = "";
 
     if (!result) {
-        reason += "username or password is incorrect";
-        encodeURI(reason)
-        redirectURL += "/login?reason="+reason;
+        failReason += "username or password is incorrect";
+        encodeURI(failReason)
+        redirectURL += "/login?reason="+failReason;
         res.redirect(redirectURL);
         return;
     }
@@ -133,13 +142,13 @@ app.post("/dologin", async function(req, res) {
     bcrypt.compare(pword, result.password, function(err, checkPassword) {
         if (checkPassword) { // Log the user in, if the password is correct
             req.session.loggedin = true;
-            req.session.uname = uname;
+            req.session.email = email;
             req.session.userLevel = result.userLevel;
             res.redirect('/profile');
         } else { // The password is wrong
-            reason += "username or password is incorrect";
-            encodeURI(reason);
-            redirectURL += "/?reason="+reason;
+            failReason += "username or password is incorrect";
+            encodeURI(failReason);
+            redirectURL += "/?reason="+failReason;
             res.redirect(redirectURL);
 
             return;
@@ -164,9 +173,9 @@ app.post("/doregister", async function(req, res) {
     let redirectURL = "";
 
     if (result) { // The user already exists
-        reason += "already exists";
-        encodeURI(reason);
-        redirectURL += "/register?reason="+reason;
+        failReason += "already exists";
+        encodeURI(failReason);
+        redirectURL += "/register?reason="+failReason;
     } else {
         // 9 rounds of salting should be enough
         bcrypt.hash(pword, 9, async function(err, hash) {
@@ -197,11 +206,53 @@ app.post("/doregister", async function(req, res) {
 
             // Log the new user in
             req.session.loggedin = true;
-            req.session.uname = uname;
+            req.session.email = email;
             req.session.userLevel = 1;
             res.redirect("/profile");
         });
     }
+});
+
+app.post("/profile", async function(req, res) {
+    // Get the user profile information
+    res.send("Hmm. You shouldn't be here! This page is still under development");
+
+    if (!req.session.loggedin) {
+        res.send({"Error": 404, "Description": "The user is not logged in, therefore profile information cannot be retrieved"});
+        return;
+    }
+
+    // Find the user
+    let result = await db.collection('Users').findOne({"email": req.session.email});
+
+    if (!result) {
+        res.send({Error: 404, Description: "The user does not exist anymore, therefore profile information cannot be retrieved"});
+        return;
+    }
+
+    // Find the user's associated placement location
+    let locationName = "Not set";
+
+    if (result.hasOwnProperty("location")) {
+        if (result.location !== undefined && result.location !== null) {
+            let locationResult = await db.collection('Locations').findOne({"_id": result.location});
+
+            if (locationResult) {
+                locationName = locationResult.name + " // " + locationResult.area;
+            } else {
+                // If there is no result from the database, then the user set a location at some point that is not valid anymore
+                locationName = "Invalid location. Please change"
+            }
+        }
+    }
+    
+    // Send the processed user information to the user
+    res.send({
+        firstName: result.firstName, 
+        lastName: result.lastName, 
+        email: result.email,
+        location: locationName
+    });
 });
 
 app.post("/douseredit", function(req, res) {
@@ -229,7 +280,3 @@ app.use(function(req, res) {
     res.status(404);
     res.render("pages/error404");
 })
-
-
-// app.listen(8080);
-// console.log("Server is running on port 8080");
